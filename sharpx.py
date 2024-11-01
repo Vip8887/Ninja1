a3db963507ceb360453fb0c140759392fd82d557
Vip8887 <nin882042@gmail.com>
2024-11-01 22:42:00 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7850149668:AAGVNLk6QfxPkf4T86lElTdRvAvc3d3lMwA")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Ninja2399")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6517590211")
+
+
+
+
+
+  
